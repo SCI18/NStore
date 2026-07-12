4d32b9fc6efa8feee5a2b47006eb48492a5aa88d
SCI18 <sitratiskayopetatalias@gmail.com>
2026-07-12 12:04:14 +0000

    test 2

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ window.STORE_CONFIG = {
   // Default language: "en" or "fr"
   defaultLang: "en"
 
-  emailJs: {
+  emailjs:{
     serviceId: "service_lkwdphi",
     templateId: "template_g8gfahq",
     publicKey: "dO8v72KmZoJqQb03P"
